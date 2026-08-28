4ec479a44743f07e0f51d7412e03c3921afd2d1b
jungmin <justin930522@gmail.com>
2026-08-28 13:56:12 +0900

    기준날짜 변경

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { CgvResponse, DateItem, Schedule } from './apiResponseTypes.js';
 import { sendDiscordMessage, sendDiscordWarning } from './discord.js';
 
 const CONFIG = {
-  baselineDate: '20260901',
+  baselineDate: '20260908',
   companyCode: 'A420',
   siteNo: '0013', // cgv 용산 iparkmall 지점
   movieNo: '30001323',
